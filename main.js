c3847d92a6df7604f10460f71475636ebfc77dd2
bryanmej <bryanmej@gmail.com>
2019-06-19 13:05:30 -0500

    added reset button and some styling

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const board = document.querySelector('#board')
 const ctx = board.getContext('2d')
 const btn = document.getElementById('start')
-
-//Interval
-let interval
+const reset = document.getElementById('reset')
 
 //Mine variables
 let x = board.width / 2 - 15
-let y = board.height - 150
-let dx = 2.5
-let dy = -2.5
+let y = board.height - 140
+let dx = 3
+let dy = -3
+
 //Block variables
 const blockRow = 3
 const blockColumns = 7
@@ -17,7 +16,7 @@ const blockWidth = 100
 const blockHeight = 20
 const blockPadd = 20
 const blockMargTop = 30
-const blockMargLeft = 70
+const blockMargLeft = 65
 let blocks = []
 for(let c = 0; c < blockColumns; c++) {
   blocks[c] = []
@@ -25,12 +24,16 @@ for(let c = 0; c < blockColumns; c++) {
     blocks[c][r] = { 
       x: 0, 
       y: 0, 
-      status: 1 
+      display: 1 
     }
   }
 }
+
 //Score variable
 let score = 0
+
+//Interval
+let interval
 
 //Classes for game components
 class Board {
@@ -41,7 +44,6 @@ class Board {
     this.height = board.height
     this.img = new Image()
     this.img.src = './Images/undersea.png'
-
     this.audio = new Audio()
     this.audio.src = 'http://23.237.126.42/ost/pokemon-gold-silver-crystal/zbkuympx/022%20Violet%20City.mp3'
 
@@ -56,9 +58,18 @@ class Board {
 
 class Willy {
   constructor(){
+    this.x = board.width / 2 - 80
+    this.y = 25
+    this.width = 250
+    this.height = 100
+    this.img = new Image()
+    this.img.src = './Images/willy.png'
     this.audio = new Audio()
     this.audio.src = 'https://www.partnersinrhyme.com/files/sounds1/MP3/animal/WhalesDS/dolphin1.mp3'
   }  
+  draw() {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+  }
 }
 
 class Mine {
@@ -117,7 +128,7 @@ class Ship {
 
 class Score {
   constructor() {
-    this.x = 5
+    this.x = 10
     this.y = board.height - 10
   }
   draw() {
@@ -127,10 +138,11 @@ class Score {
   }
 }
 
+//helper functions
 function drawBlocks() {
   for(let c = 0; c < blockColumns; c++) {
     for(let r = 0; r < blockRow; r++) {
-      if(blocks[c][r].status === 1) {
+      if(blocks[c][r].display === 1) {
         let blockX = (c * (blockWidth + blockPadd)) + blockMargLeft
         let blockY = (r * (blockHeight + blockPadd)) + blockMargTop
         blocks[c][r].x = blockX
@@ -149,10 +161,10 @@ function collisionDetect() {
   for(let c = 0; c < blockColumns; c++) {
     for(let r = 0; r < blockRow; r++) {
       let b = blocks[c][r]
-      if(b.status === 1) {
-        if(mine.x > b.x && mine.x < b.x + blockWidth && mine.y > b.y && mine.y < b.y + blockHeight) {
+      if(b.display === 1) {
+        if(mine.x > b.x + 5 && mine.x < b.x + blockWidth && mine.y > b.y - 50 && mine.y < b.y + blockHeight) {
           dy = -dy
-          b.status = 0
+          b.display = 0
           score++
           if(score === blockColumns * blockRow) {
             bgboard.audio.pause()
@@ -166,6 +178,7 @@ function collisionDetect() {
   }
 }
 
+//Instances
 const bgboard = new Board()
 const ship = new Ship()
 const mine = new Mine()
@@ -175,6 +188,7 @@ const willy = new Willy()
 function update() {
   ctx.clearRect(0, 0, board.width, board.height)
   bgboard.draw()
+  willy.draw()
   mine.draw()
   ship.draw()
   drawBlocks()
@@ -182,16 +196,17 @@ function update() {
   points.draw()
 }
 
+
+//Listeners
 bgboard.audio.addEventListener('ended', function() {
   this.currentTime = 0
   this.play()
 }, false)
 
-
 document.addEventListener('keydown', (e) => {
-  if(e.keyCode === 39) {
+  if(e.keyCode === 39 || e.keyCode === 68) {
      ship.moveRight()
-  } else if (e.keyCode === 37) {
+  } else if (e.keyCode === 37 || e.keyCode === 65) {
     ship.moveLeft()
   } 
 })
@@ -199,6 +214,10 @@ document.addEventListener('keydown', (e) => {
 btn.onclick = () => {
   bgboard.audio.play() 
   interval = setInterval(update, 10)
+}
+
+reset.onclick = () => {
+  document.location.reload()
 }
 
 
